aea0ef876ce9acd0ea6fc236fde7f4dcd2e999c9
Mohammad Al-Abedredha <malabedredha@gmail.com>
2023-06-14 22:18:12 -0500

    user response questions

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const fs = require("fs")
-let SVG = require("./lib/svg")
+const { Circle, Triangle, Square } = require("./lib/shapes");
+const fs = require("fs");
+const SVG = require("./lib/svg");
+const { writeFile } = require("fs");
 
-
-let example = new SVG("AND", "green", "triangle", "yellow");
-
-
-
-fs.writeFile("shape.svg", example.markup, function (err) {
-    if (err)
-        console.log(err)
-})
+const userResponses = await inquirer.prompt([
+  {
+    type: "list",
+    name: "shape",
+    message: "choose a shape:",
+    choices: ["circle", "triangle", "square"],
+  },
+  {
+    type: "input",
+    name: "fill",
+    message: "What color do you want it to be"
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "What text do you want inside of your shape? (Must be between 1-3 characters)",
+    validate: (input) => {
+      if (input.length > 3) {
+        return "(must be between 1-3 characters";
+      }
+      return true;
+    },
+  },
+ 
